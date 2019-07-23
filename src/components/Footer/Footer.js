a5632c39731881dc6ee9c05a9a9e1e8a2f49328a
79manuel <prezcabellomanuel>
2019-07-23 11:14:59 -0700

    Refactored footer for laptop

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,15 +17,15 @@ class Footer extends React.Component {
             <div className='footer__info'>
               <div>
                 <p>
-                  <span>Squires Four Public House</span><br/><br/>
-                   6301 Stickle Road Vernon.<br/>
-                   <a href='tel: 2505492144'>Phone: 250-549-2144</a><br/>
-                  <a href='mailto: info@squiresfourpub.com'>Email: info@squiresfourpub.com</a><br/>
+                  <span>Squires Four Public House</span>
+                   6301 Stickle Road Vernon.
+                   <a href='tel: 2505492144'>Phone: 250-549-2144</a>
+                  <a href='mailto: info@squiresfourpub.com'>Email: info@squiresfourpub.com</a>
                    </p>
                 <p>
-                  <span>Squires Four Cold Beer & Wine Store</span><br/><br/>
-                  6301 Stickle Road Vernon<br/>
-                  <a href='tel: 2505492226'>Phone: 250-549-2226</a><br/>
+                  <span>Squires Four Cold Beer & Wine Store</span>
+                  6301 Stickle Road Vernon
+                  <a href='tel: 2505492226'>Phone: 250-549-2226</a>
                 </p>
               </div>
               <p className='footer__copyright'>Squires Four Public House &copy; 2019</p>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -9,19 +9,19 @@ class Contact extends React.Component {
                   <h1>Contact</h1>
                   <div className='contact-info'>
                     <p>
-                      <span>Squires Four Public House</span><br/><br/>
-                      6301 Stickle Road Vernon.<br/>
-                      <a href='tel: 2505492144'>Phone: 250-549-2144</a><br/>
+                      <span>Squires Four Public House</span><br/>
+                      6301 Stickle Road Vernon.
+                      <a href='tel: 2505492144'>Phone: 250-549-2144</a>
                       <a href='mailto: info@squiresfourpub.com'>Email: info@squiresfourpub.com</a><br/>
                     </p>
                     <p>
-                      <span>Squires Four Cold Beer & Wine Store</span><br/><br/>
-                      6301 Stickle Road Vernon<br/>
+                      <span>Squires Four Cold Beer & Wine Store</span><br/>
+                      6301 Stickle Road Vernon
                       <a href='tel: 2505492226'>Phone: 250-549-2226</a><br/>
                     </p>
                   </div>
                   <iframe
-                      title=''
+                      title='/'
                       src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d20389.55232346327!2d-119.2630888!3d50.2976389!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537dd879dd685a97%3A0xf413a29ff8fbfaee!2s6301+Stickle+Rd%2C+Vernon%2C+BC+V1B+3R4!5e0!3m2!1sen!2sca!4v1563228800695!5m2!1sen!2sca"
                       frameBorder="0"
                       allowFullScreen>
